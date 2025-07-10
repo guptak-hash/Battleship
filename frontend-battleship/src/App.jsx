ca40fc2606a2d43e1601e2f21f6ac2f32b4fc014
Saurabh Kumar <44322770+ksaurabh252@users.noreply.github.com>
2025-07-11 03:24:24 +0530

    Refactor App.js: Setup React Router and added Home route

--- frontend-battleship/src/App.jsx
+++ frontend-battleship/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
