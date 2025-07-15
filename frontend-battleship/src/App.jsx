6c7f8b072e60199e5dc33ca01a223338fd1dc039
guptak-hash <guptakishan1123@gmail.com>
2025-07-15 23:39:21 +0530

    deplyoed

--- frontend-battleship/src/App.jsx
+++ frontend-battleship/src/App.jsx
@@ -1,32 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import GameRules from './pages/GameRules';
-// import About from './pages/About';
-// import SinglePlayer from './pages/SinglePlayer';
-// import GamePage from './pages/GamePage';
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path='/rules' element={<GameRules/>}/>
-//         <Route path='/about' element={<About/>}/>
-//         <Route path='/singleplayer' element={<SinglePlayer/>}/>
-//         <Route path='/game' element={<GamePage/>}/>
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
