37b7fb5ffb75b8022ae8344ea181f07ef70a0227
Saurabh Kumar <44322770+ksaurabh252@users.noreply.github.com>
2025-07-15 15:22:33 +0530

    profile added

--- frontend-battleship/src/pages/About.jsx
+++ frontend-battleship/src/pages/About.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import { FaGithub } from 'react-icons/fa';
-
+import KishanImage from '../assets/Images/Kishan_Gupta.png'
+import SaurabhImage from '../assets/Images/saurabhKumar.jpeg'
 const developers = [
   {
-    name: "Alexandros Oikonomou",
+    name: "Jagan Mohan Reddy",
     image: "/img/alex.jpg",
     github: "https://github.com/alexoiik",
   },
   {
-    name: "Vasilios Mourtzios",
-    image: "/img/billy.jpg",
-    github: "https://github.com/ssjbillys",
+    name: "Kishan Gupta",
+    image: KishanImage,
+    github: "https://github.com/guptak-hash",
   },
   {
-    name: "Stylianos Panagiotopoulos",
-    image: "/img/stelios.jpg",
-    github: "https://github.com/steliospanag01",
+    name: "Saurabh Kumar",
+    image: SaurabhImage,
+    github: "https://github.com/ksaurabh252",
   },
-  {
-    name: "Kostas Kyriakos Batsios",
-    image: "/img/kostas.jpg",
-    github: "https://github.com/KostasKyriakosBatsios",
-  },
+
 ];
 
 function About() {
@@ -33,21 +30,18 @@ function About() {
       <section className="max-w-5xl mx-auto px-4 py-10">
         <h4 className="text-lg text-sky-300">Battleship Game</h4>
         <h1 className="text-4xl font-bold mb-6">DEVELOPERS</h1>
-        
+
         <p className="mb-10 leading-relaxed">
-          We are 4 young undergraduate students with a passion for gaming and web development 
+          We are 3 students with a passion for MERN stack development
           that teamed up to create an innovative web game. This game is a strong full-stack
-          project and created for our university's course "Development of Web Systems and Applications" 
-          at the department of{" "}
-          <a href="https://www.iee.ihu.gr/en/" className="text-sky-300 underline" target="_blank" rel="noreferrer">
-            IEE
-          </a>. Together, our collaboration resulted in a captivating, enjoyable and user-friendly{" "}
+          project and created for our university's course "Development of Web Systems and Applications"
+          at the department of Together, our collaboration resulted in a captivating, enjoyable and user-friendly{" "}
           <a href="/" className="text-sky-300 underline" target="_blank" rel="noreferrer">
             Online Battleship Game!
           </a>
         </p>
 
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4  ms-25">
           {developers.map((dev, index) => (
             <div
               key={index}
