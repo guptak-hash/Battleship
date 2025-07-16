aa51f7690faad13abb699cf019ec2f07e1881b52
guptak-hash <guptakishan1123@gmail.com>
2025-07-16 12:00:49 +0530

    dod some change

--- backend-battleship/models/Game.js
+++ backend-battleship/models/Game.js
@@ -1,36 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const gameSchema = new mongoose.Schema({
-//   player: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true
-//   },
-//   opponentType: {
-//     type: String,
-//     enum: ['computer', 'human'],
-//     required: true
-//   },
-//   result: {
-//     type: String,
-//     enum: ['win', 'loss', 'draw', 'incomplete'],
-//     required: true
-//   },
-//   duration: {
-//     type: Number, // in seconds
-//     required: true
-//   },
-//   datePlayed: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// module.exports = mongoose.model('Game', gameSchema);
-
-
-
-
 const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema({
